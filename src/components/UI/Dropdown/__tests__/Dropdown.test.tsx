fe4b80260896f1d91129ded8819568d34e73d46a
Matheus Riboli <matheusvor@gmail.com>
2025-06-07 11:26:20 -0300

    fix: fixed dropdown import inside the test file

--- src/components/UI/Dropdown/__tests__/Dropdown.test.tsx
+++ src/components/UI/Dropdown/__tests__/Dropdown.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { render, screen, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Dropdown } from '~/components'
+import Dropdown from '../Dropdown'
 
 interface MockInputProps {
   value: string
